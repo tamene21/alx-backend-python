e7417444c0a40f138996f9fb709089f1c69d072b
tamene gelaye <tamenes21@gmail.com>
2023-07-06 16:18:18 +0000

    task 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,13 +7,13 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """create copies"""
     zoomed_in: List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
