87eb5724e54f940b4ce0999b25310cf8133cc170
tamene gelaye <tamenes21@gmail.com>
2023-07-06 13:49:34 +0000

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Complex variable annonated"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums a list of float"""
+
+    return sum(input_list)
