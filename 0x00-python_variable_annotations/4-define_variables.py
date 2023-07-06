b70a25fe2acd5a57b1134063da93bf3d0ed4b1c5
tamene gelaye <tamenes21@gmail.com>
2023-07-06 13:31:40 +0000

    task 4 edited

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """declaring annotated variable"""
 
 
-a: int = 1,
-pi: float = 3.14,
-i_understand_annotations: boolean = True,
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boolean = True
 school: str = "Holberton"
