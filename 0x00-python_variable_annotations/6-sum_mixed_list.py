eeb4071cc6e836fa73a868df304c43ce16fde885
tamene gelaye <tamenes21@gmail.com>
2023-07-06 14:06:28 +0000

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains mixed-list list of integers and float & return a sum as a float"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Return a sum of int and float values as float"""
+    return sum(mxd_lst)
