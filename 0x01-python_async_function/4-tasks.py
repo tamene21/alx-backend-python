216332bfea049310bfad12c6136b24e65eb06a29
tamene gelaye <tamenes21@gmail.com>
2023-07-10 22:33:35 +0000

    task 4 corrected

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List(float):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """task wait with max_delay
     Args:
         n (int): n times
